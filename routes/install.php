443f00ca33f4da89a950cde30740dff5880e0f82
Nejc <nejc.cotic@gmail.com>
2020-03-29 20:08:55 +0200

    first step to testing and bugfixes

--- routes/install.php
+++ routes/install.php
@@ -1,9 +1,9 @@
 <?php
 
-Route::prefix('installation')->name('installation.')->group(function () {
-	Route::get('', 'Installation\InstallationController@greetings')->name('greetings');
-	Route::get('finish', 'Installation\InstallationController@finish')->name('finish');
-	Route::resource('config', 'Installation\ConfigController')->only(['index', 'store']);
-	Route::resource('database', 'Installation\DatabaseController')->only(['index', 'store']);
-	Route::resource('accounts', 'Installation\AccountController')->only(['index', 'store']);
+Route::prefix('installation')->namespace('installation')->name('installation.')->group(function () {
+	Route::get('', 'InstallationController@greetings')->name('greetings');
+	Route::get('finish', 'InstallationController@finish')->name('finish');
+	Route::resource('config', 'ConfigController')->only(['index', 'store']);
+	Route::resource('database', 'DatabaseController')->only(['index', 'store']);
+	Route::resource('accounts', 'AccountController')->only(['index', 'store']);
 });
