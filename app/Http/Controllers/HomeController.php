13c8d1f4c708283faeea9bdc9e8a830a2f8e42bf
Nejc <nejc.cotic@gmail.com>
2020-03-29 16:52:37 +0200

    Update localization

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Languages;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,8 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $languages = Languages::all();
         $village = [];
-        return view('fields', compact('village'));
+        return view('homepage', compact(['village', 'languages']));
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('homepage');
-})->name('index');
+Route::get('/', 'HomeController@index')->name('index');
 
-Route::get('locale/{locale}', function ($locale){
+Route::get('locale/{locale}/{id}', function ($locale, $id){
     Session::put('locale', $locale);
+    Session::put('locale_id', $id);
     return redirect()->back();
 });
 
